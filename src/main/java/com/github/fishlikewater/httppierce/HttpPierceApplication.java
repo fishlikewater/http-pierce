958a9af610e031ae74b9aa13c126e22e68271eee
fishlikewater <fishlikewater@126.com>
2023-02-08 20:27:56 +0800

    Initial commit

---
+++ src/main/java/com/github/fishlikewater/httppierce/HttpPierceApplication.java
@@ -0,0 +1,13 @@
+package com.github.fishlikewater.httppierce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpPierceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpPierceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/fishlikewater/httppierce/HttpPierceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fishlikewater.httppierce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpPierceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
