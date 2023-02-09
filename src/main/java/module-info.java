42cf8dbfdf48522b9a0726537a73cff5dd8b1658
fishlikewater <fishlikewater@126.com>
2023-02-09 23:09:00 +0800

    添加模块化

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * <p>
+ *  模块化配置文件
+ * </p>
+ *
+ * @author fishlikewater@126.com
+ * @since 2023年02月09日 23:03
+ **/
+module http.pierce {
+
+    requires spring.boot;
+    requires spring.boot.starter;
+    requires spring.boot.autoconfigure;
+    requires io.netty.all;
+    requires lombok;
+    requires hutool.all;
+}
