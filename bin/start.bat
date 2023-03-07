5e574871366005e2e5404ef1a65bb7f4cebe21b1
fish <fishlikewater@126.com>
2023-03-07 15:35:33 +0800

    Streamline project dependencies

--- bin/start.bat
+++ bin/start.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist "JRE\bin\java.exe" echo Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk8 or later is better! & pause & EXIT
-set "JAVA=JRE\bin\java.exe"
+if not exist "jre\bin\java.exe" echo Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk17 or later is better! & pause & EXIT
+set "JAVA=jre\bin\java.exe"
 
 set SERVER=http-pierce
 
